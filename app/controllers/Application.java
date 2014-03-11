cb86485f728cbfc417852ef54c7d02a65015233d
LeticiaLBarbosa <leticiabarbosa94@gmail.com>
2014-03-11 15:15:03 -0300

    ponto e virgula a mais

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,10 @@
 package controllers;
 
-//import Exceptions.LimitesExcedidosException;
-//import Exceptions.PreRequisitosInsuficientesException;
-//import models.*;
-//import play.data.DynamicForm;
-//import play.data.Form;
+import Exceptions.LimitesExcedidosException;
+import Exceptions.PreRequisitosInsuficientesException;
+import models.*;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.*;
 import views.html.*;
 
